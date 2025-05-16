ddebf2168f8463e03186ba092d1195e8180c5fd7
Bermet <iskenderova.bermet19@gmail.com>
2025-05-16 15:03:36 +0600

    initial commit

---
+++ src/main/java/kg/alatoo/broke/BrokeApplication.java
@@ -0,0 +1,13 @@
+package kg.alatoo.broke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrokeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kg/alatoo/broke/BrokeApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.alatoo.broke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
